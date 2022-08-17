66b3256e6b10e0b5376c6a51c02d3d283850fd7b
Adaeze Ndupu <andupu4@gmail.com>
2022-08-17 02:07:26 +0100

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,22 +26,21 @@ void bubble_sort(int *array, size_t size)
 	size_t i, len = size;
 	bool bubbly = false;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
-	while (len > 1)
+
+	while (bubbly == false)
 	{
+		bubbly = true;
 		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap_ints(&array[i], &array[i + 1]);
-				bubbly = true;
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
 			}
 		}
-		if (!bubbly)
-			break;
-		bubbly = false;
 		len--;
-		print_array(array, size);
 	}
 }
